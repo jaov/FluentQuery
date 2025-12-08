f9f3795d9ad13173b35a5ca6f3ad5a0902bd5046
jesuso <j.ochoa@norteconecta.net>
2025-12-08 11:33:45 -0400

    Added default methods for lazy people that wouldn't have dealt with the exception anyway.

--- src/main/java/dev/oveja/jdbc/fluent/interfaces/select/SelectExecute.java
+++ src/main/java/dev/oveja/jdbc/fluent/interfaces/select/SelectExecute.java
@@ -1,11 +1,20 @@
 package dev.oveja.jdbc.fluent.interfaces.select;
 
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 public interface SelectExecute<T>{
     List<T> execute() throws SQLException;
+
+    default List<T> executeOrEmpty() {
+        try {
+            return execute();
+        } catch (SQLException e) {
+            return Collections.emptyList();
+        }
+    }
 
     default Optional<T> fetchOne() throws SQLException {
         List<T> results =  execute();
@@ -16,4 +25,13 @@ public interface SelectExecute<T>{
 
         return results.stream().findFirst();
     }
+
+    default Optional<T> fetchOneOrEmpty() {
+        try {
+            return fetchOne();
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
 }
